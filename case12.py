9a1478ac8e1a4711f5e2877e6eeb363ec5b54eef
Alexandr Selishchev <44421225+selishchev@users.noreply.github.com>
2019-03-25 16:57:24 +0700

    Add files via upload

--- case12.py
+++ case12.py
@@ -1,1 +1,21 @@
 import os
+
+
+def countFiles(path=os.getcwd()):
+    sum = 0
+    for i, j, k in os.walk(path):
+        sum += len(k)
+    return sum
+
+
+def countBytes(path=os.getcwd()):
+    bytes = 0
+    for i, j, k in os.walk(path):
+        for p in k:
+            if os.path.isfile(i + '/' + p):
+                bytes += os.path.getsize(i + '/' + p)
+    return bytes
+
+
+print(countFiles())
+print(countBytes())
