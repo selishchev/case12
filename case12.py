a29cc6b440e868bb8e2eaad44e528b890d789a77
arkroft <44421320+arkroft@users.noreply.github.com>
2019-03-25 20:44:08 +0700

    Update case12.py

--- case12.py
+++ case12.py
@@ -1,4 +1,31 @@
 import os
+import os.path
+
+def moveUp():
+    a = os.getcwd()
+    b = os.path.split(a)
+    os.chdir(b[0])
+    print(os.getcwd())
+
+
+def moveDown(currentDir):
+    Dir = os.getcwd()
+    a = os.path.join(Dir, currentDir)
+    if os.path.exists(a):
+        b = os.path.join(Dir, currentDir)
+        os.chdir(b)
+        print(os.getcwd())
+    else:
+        print('Ошибка')
+
+n = int(input())
+if n == 1:
+    moveUp()
+elif n == 2:
+    a = input()
+moveDown(a)
+
+
 MENU = '1. Просмотр каталога'+'\n'+'2. На уровень вверх'+'\n'\
  '3. На уровень вниз'+'\n'+'4. Количество файлов и каталогов'+'\n'\
  '5. Размер текущего каталога (в байтах)'+'\n'+'6. Поиск файла'+'\n'\
