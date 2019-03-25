ee7da16cd767dbbee249105fe7d883a9b39405f0
Кирилл Паймушкин (Kirill Paymushkin) <44719713+paymtime@users.noreply.github.com>
2019-03-25 17:20:10 +0700

    Add files via upload

--- case12.py
+++ case12.py
@@ -1,1 +1,26 @@
 import os
+import os.path
+
+def moveUp():
+    a = os.getcwd()
+    b = os.path.split(a)
+    os.chdir(b[0])
+    print(os.getcwd())
+
+
+def moveDown(currentDir):
+    Dir = os.getcwd()
+    a = os.path.join(Dir, currentDir)
+    if os.path.exists(a):
+        b = os.path.join(Dir, currentDir)
+        os.chdir(b)
+        print(os.getcwd())
+    else:
+        print('Ошибка')
+
+n = int(input())
+if n == 1:
+    moveUp()
+elif n == 2:
+    a = input()
+    moveDown(a)
